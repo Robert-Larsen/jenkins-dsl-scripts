7e43310a6322342a7be85a3f5f11e0b380f1ea1c
Sissel Fladby <sissel.fladby@nav.no>
2018-01-11 12:23:26 +0100

    Bytte til riktig miljøvariabel

--- informasjon_og_veiledning/bygge-pipeline.groovy
+++ informasjon_og_veiledning/bygge-pipeline.groovy
@@ -9,7 +9,7 @@ notifierDockerImage = "${pusDockerImagePrefiks}notifier"
 policyDockerImage = "${pusDockerImagePrefiks}policy-validator"
 sblDockerImagePrefiks = "docker.adeo.no:5000/sbl/"
 
-miljo = "q1"
+miljo = "q9"
 
 appConfig = "app-config.yaml"
 dockerfile = "Dockerfile"
