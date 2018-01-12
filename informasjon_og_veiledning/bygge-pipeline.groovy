3c30e28d2d215105fd720a2269ba992faa95d996
Sissel Fladby <sissel.fladby@nav.no>
2018-01-12 10:54:46 +0100

    Fjerning av cleanup() siden funksjonen ikke finnes

--- informasjon_og_veiledning/bygge-pipeline.groovy
+++ informasjon_og_veiledning/bygge-pipeline.groovy
@@ -240,7 +240,7 @@ gitCommitHash=${gitCommitHash}
                 stage("nais deploy ${miljo}") {
 
                     sh("curl -v -X PUT --upload-file app-config.yaml https://${REPO_USERNAME}:${REPO_PASSWORD}@repo.adeo.no/repository/raw/nais/${applikasjonsNavn}/${versjon}/nais.yaml")
-                    
+
                     sh("docker run" +
                             " --rm" +  // slett container etter kjøring
                             " --env-file ${environmentFile}" +
@@ -273,5 +273,4 @@ gitCommitHash=${gitCommitHash}
         status("error")
         throw t
     }
-    cleanup()
 }
